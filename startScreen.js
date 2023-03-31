4f9e76478ea651a71be75ca3dda62fe272e373c7
elisabettagianii <elisabettagiani@yahoo.com>
2023-03-31 17:01:36 +0200

    start screen
    all elements are there, we need to fix the position and maybe the font

--- startScreen.js
+++ startScreen.js
@@ -1,4 +1,37 @@
 // creating the canva
 function setup() {
-    createCanvas(1200, 600);
-  }
+  createCanvas(1200, 600);
+  background(0, 0, 0);
+  // start button
+  button = createButton("START PLAYING");
+  button.position(530, 320);
+  button.style("color:red");
+  button.style("background-color:black");
+  button.style("border", "none");
+  button.size(250, 110);
+
+  button.style("font-size", "45px");
+
+  // button.mousePressed(levelOne);
+}
+
+function startScreen() {
+  // text: timemachine
+  fill(255, 255, 255);
+  textSize(90);
+  // textFont(Copperplate);
+  textAlign(CENTER, TOP);
+  text("TIMEMACHINE", 590, 100);
+// text: history
+fill(255, 255, 255);
+textSize(15);
+text("History", 900, 500, 100, 100);
+// text: instructions 
+fill(255, 255, 255);
+textSize(15);
+text("Instructions", 900, 520, 100, 100);
+}
+
+function draw() {
+  startScreen();
+}
