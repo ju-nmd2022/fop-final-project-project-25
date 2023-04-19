1a949d0f7c1adf2d55e1b8c040df8341c7dcd9c8
Aseul11 <asul22qa@student.ju.se>
2023-04-19 19:07:41 +0200

    simplified levels

--- finalgame.js
+++ finalgame.js
@@ -1,5 +1,5 @@
 let isGameActive = true;
-let state = "start";
+let state = "levelOne";
 
 function setup() {
   createCanvas(1200, 600);
@@ -47,11 +47,8 @@ function obstaclesOne(x, y) {
   triangle(x + 330, y + 430, x + 350, y + 430, x + 340, y + 410);
   triangle(x + 350, y + 430, x + 370, y + 430, x + 360, y + 410);
   //
-  triangle(x + 900, y + 250, x + 920, y + 250, x + 910, y + 230);
-  triangle(x + 920, y + 250, x + 940, y + 250, x + 930, y + 230);
-  //
-  triangle(x + 960, y + 280, x + 980, y + 280, x + 970, y + 300);
-  triangle(x + 980, y + 280, x + 1020, y + 280, x + 1000, y + 330);
+  triangle(x + 1040, y + 300, x + 1020, y + 300, x + 1030, y + 280);
+  triangle(x + 1000, y + 300, x + 1020, y + 300, x + 1010, y + 280);
 
   pop();
 }
@@ -71,26 +68,6 @@ function obstaclesTwo(x, y) {
   pop();
 }
 
-function obstaclesThree(x, y) {
-  push();
-  fill(200, 200, 200);
-  // 1
-  triangle(x + 380, y + 220, x + 400, y + 220, x + 390, y + 200);
-  triangle(x + 400, y + 220, x + 420, y + 220, x + 410, y + 200);
-  // 2
-  triangle(x + 550, y + 220, x + 570, y + 220, x + 560, y + 200);
-  triangle(x + 570, y + 220, x + 590, y + 220, x + 580, y + 200);
-  // 3
-  triangle(x + 500, y + 490, x + 520, y + 490, x + 510, y + 510);
-  triangle(x + 520, y + 490, x + 540, y + 490, x + 530, y + 510);
-  triangle(x + 540, y + 490, x + 560, y + 490, x + 550, y + 510);
-  triangle(x + 560, y + 490, x + 580, y + 490, x + 570, y + 510);
-  triangle(x + 580, y + 490, x + 600, y + 490, x + 590, y + 510);
-  triangle(x + 600, y + 490, x + 620, y + 490, x + 610, y + 510);
-  triangle(x + 620, y + 490, x + 640, y + 490, x + 630, y + 510);
-  pop();
-}
-
 /////////LIQUID//////////
 
 function liquidOne(x, y) {
@@ -100,7 +77,7 @@ function liquidOne(x, y) {
   noStroke();
   drawingContext.shadowBlur = 5;
   drawingContext.shadowColor = "red";
-  rect(x + 600, y + 285, 170, 30);
+  rect(x + 700, y + 305, 200, 30);
   rect(x + 180, y + 555, 200, 30);
   rect(x + 750, y + 555, 230, 30);
   pop();
@@ -116,21 +93,6 @@ function liquidTwo(x, y) {
 
   drawingContext.shadowColor = "green";
   rect(x + 880, y + 225, 170, 30);
-  rect(x + 100, y + 555, 1000, 30);
-
-  pop();
-}
-
-function liquidThree(x, y) {
-  // liquid
-
-  push();
-  fill(255, random(190, 215), 0);
-  noStroke();
-  drawingContext.shadowBlur = 5;
-  drawingContext.shadowColor = "orange";
-  rect(x + 790, y + 175, 200, 30);
-  rect(x + 450, y + 385, 200, 30);
   rect(x + 100, y + 555, 1000, 30);
 
   pop();
@@ -199,35 +161,6 @@ function borderTwo(x, y) {
   endShape();
 }
 
-function borderThree(x, y) {
-  push();
-  drawingContext.shadowBlur = 2;
-  drawingContext.shadowColor = "white";
-
-  strokeWeight(1);
-  // border
-  fill(200, 200, 200);
-  rect(x, y, 40, 600);
-  rect(x + 1160, y, 40, 600);
-  rect(x, y, 1200, 50);
-  //
-  beginShape();
-  vertex(x, y + 550);
-  //
-  vertex(x + 240, y + 550);
-  vertex(x + 260, y + 580);
-  vertex(x + 800, y + 580);
-  vertex(x + 820, y + 550);
-  //
-  vertex(x + 1200, y + 550);
-  vertex(x + 1200, y + 600);
-  vertex(x, y + 600);
-  vertex(x, y + 550);
-
-  endShape();
-  pop();
-}
-
 /////////MAPS//////////
 
 function mapOne(x, y) {
@@ -237,48 +170,37 @@ function mapOne(x, y) {
   //map left
 
   beginShape();
-  vertex(x + 40, y + 320);
-  vertex(x + 100, y + 320);
-  vertex(x + 100, y + 390);
-  vertex(x + 150, y + 390);
-  vertex(x + 150, y + 430);
-  vertex(x + 600, y + 430);
-  vertex(x + 600, y + 460);
-  vertex(x + 650, y + 460);
-  vertex(x + 650, y + 480);
-  vertex(x + 530, y + 480);
-  vertex(x + 530, y + 460);
-  vertex(x + 40, y + 460);
-  vertex(x + 40, y + 320);
+
+  rect(x + 40, y + 430, 410, 30, 1);
   endShape();
 
   //map right
 
   beginShape();
-  vertex(x + 520, y + 320);
-  vertex(x + 580, y + 320);
-  vertex(x + 580, y + 280);
+  vertex(x + 520, y + 330);
+  vertex(x + 580, y + 330);
+  vertex(x + 580, y + 300);
   //
-  vertex(x + 620, y + 280);
-  vertex(x + 640, y + 310);
-  vertex(x + 750, y + 310);
-  vertex(x + 770, y + 280);
+  vertex(x + 700, y + 300);
+  vertex(x + 720, y + 330);
+  vertex(x + 880, y + 330);
+  vertex(x + 900, y + 300);
 
   //
-  vertex(x + 820, y + 280);
-  vertex(x + 820, y + 250);
-  vertex(x + 1160, y + 250);
-  vertex(x + 1160, y + 280);
-  vertex(x + 920, y + 280);
-  vertex(x + 920, y + 340);
-  vertex(x + 520, y + 340);
-  vertex(x + 520, y + 320);
+  vertex(x + 900, y + 300);
+
+  vertex(x + 1160, y + 300);
+  vertex(x + 1160, y + 360);
+  vertex(x + 920, y + 360);
+  vertex(x + 920, y + 360);
+  vertex(x + 520, y + 360);
+  vertex(x + 520, y + 330);
 
   endShape();
 
   //map
   rect(x + 835, y + 520, 80, 20, 1);
-  rect(x + 665, y + 250, 60, 20, 1);
+  rect(x + 760, y + 270, 80, 20, 1);
   pop();
 }
 
@@ -297,13 +219,9 @@ function mapTwo(x, y) {
   vertex(x + 420, y + 600);
   vertex(x + 480, y + 600);
   vertex(x + 480, y + 380);
-  vertex(x + 530, y + 380);
-  vertex(x + 530, y + 350);
-  vertex(x + 600, y + 350);
+  vertex(x + 600, y + 380);
   vertex(x + 600, y + 450);
-  vertex(x + 730, y + 450);
-  vertex(x + 730, y + 500);
-  vertex(x + 770, y + 500);
+  vertex(x + 770, y + 450);
   vertex(x + 770, y + 600);
   vertex(x + 220, y + 600);
   endShape();
@@ -330,67 +248,6 @@ function mapTwo(x, y) {
   endShape();
 }
 
-function mapThree(x, y) {
-  push();
-  strokeWeight(1);
-  fill(200, 200, 200);
-
-  //bottom map
-  beginShape();
-  vertex(x + 120, y + 380);
-  //
-  vertex(x + 480, y + 380);
-  vertex(x + 500, y + 410);
-  vertex(x + 520, y + 410);
-  vertex(x + 540, y + 380);
-  //
-  vertex(x + 590, y + 380);
-  vertex(x + 610, y + 410);
-  vertex(x + 630, y + 410);
-  vertex(x + 650, y + 380);
-  //
-  vertex(x + 700, y + 380);
-  vertex(x + 700, y + 450);
-  vertex(x + 890, y + 450);
-  vertex(x + 890, y + 510);
-  vertex(x + 950, y + 510);
-  vertex(x + 950, y + 550);
-  vertex(x + 850, y + 550);
-  vertex(x + 850, y + 490);
-  vertex(x + 440, y + 490);
-  vertex(x + 440, y + 415);
-  vertex(x + 120, y + 415);
-  vertex(x + 120, y + 380);
-  endShape();
-
-  //map
-  rect(x + 855, y + 140, 80, 20, 1);
-  rect(x + 40, y + 280, 80, 25, 1);
-  rect(x + 40, y + 490, 120, 25, 1);
-  pop();
-
-  //top map
-  beginShape();
-  fill(200, 200, 200);
-  vertex(x + 190, y + 255);
-  vertex(x + 190, y + 220);
-  vertex(x + 700, y + 220);
-  vertex(x + 700, y + 170);
-  //
-  vertex(x + 800, y + 170);
-  vertex(x + 820, y + 200);
-  vertex(x + 970, y + 200);
-  vertex(x + 990, y + 170);
-  //
-  vertex(x + 1080, y + 170);
-  vertex(x + 1080, y + 100);
-  vertex(x + 1160, y + 100);
-  //
-  vertex(x + 1160, y + 255);
-  vertex(x + 640, y + 255);
-  endShape();
-}
-
 /////////DOORS//////////
 
 function doorOne(x, y) {
@@ -411,17 +268,6 @@ function doorTwo(x, y) {
   fill(111, 85, 55);
   rect(x + 1020, y + 450, 90, 100);
   fill(131, 95, 55);
-  rect(x + 1030, y + 460, 70, 90);
-  pop();
-}
-
-function doorThree(x, y) {
-  push();
-  drawingContext.shadowBlur = 5;
-  drawingContext.shadowColor = "white";
-  fill(200, 200, 200);
-  rect(x + 1020, y + 450, 90, 100);
-  fill(150, 150, 150);
   rect(x + 1030, y + 460, 70, 90);
   pop();
 }
@@ -467,19 +313,6 @@ function levelTwo() {
   startButton.hide();
 }
 
-function levelThree() {
-  push();
-  noStroke();
-  background(0, 0, 0);
-  liquidThree(0, 0);
-  pop();
-  mapThree(0, 0);
-  borderThree(0, 0);
-  obstaclesThree(0, 0);
-  doorThree(0, 0);
-  startButton.hide();
-}
-
 function winScreen() {
   // text: YOU WON!
   fill(255, 255, 255);
@@ -520,8 +353,6 @@ function mouseClicked() {
   } else if (state === "levelOne") {
     state = "levelTwo";
   } else if (state === "levelTwo") {
-    state = "levelThree";
-  } else if (state === "levelThree") {
     state = "win";
   } else if (state === "win") {
     state = "fail";
@@ -539,8 +370,6 @@ function draw() {
     levelOne();
   } else if (isGameActive && state === "levelTwo") {
     levelTwo();
-  } else if (isGameActive && state === "levelThree") {
-    levelThree();
   } else if (isGameActive === false && state === "win") {
     winScreen();
   } else if (isGameActive === false && state === "fail") {
