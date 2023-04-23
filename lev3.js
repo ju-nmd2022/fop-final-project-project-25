cc8239ef3dc02d594eaab1bc8b3cdaab4ad50921
Aseul11 <asul22qa@student.ju.se>
2023-04-23 14:18:11 +0200

    changes

--- lev3.js
+++ lev3.js
@@ -1,3 +1,24 @@
+let stage = 0;
+let jump = false;
+let direction = 1; //forse of gravity in the y direction
+let velocity = 2; // the speed of character
+let jumpPower = 11; //how high character jumps
+let fallingSpeed = 4; //equal to velocity
+let minHeight = 490; // height of ground
+let maxHeight = 50; // height of sky
+let jumpCounter = 0;
+//////////////
+//////////////boxes variables////////////////
+let p1X = 100; //position X for player
+let p1Y = 490;
+let pWidth = 30;
+let pHeight = 60;
+
+let b1X = 210;
+let b1Y = 450;
+let bWidth = 240;
+let bHeight = 30;
+
 function setup() {
   createCanvas(1200, 600);
 }
@@ -11,10 +32,6 @@ function obstaclesThree(x, y) {
   // 2
   triangle(x + 880, y + 400, x + 900, y + 400, x + 890, y + 380);
   triangle(x + 900, y + 400, x + 920, y + 400, x + 910, y + 380);
-  // 3
-  triangle(x + 1000, y + 400, x + 1020, y + 400, x + 1010, y + 380);
-  triangle(x + 1020, y + 400, x + 1040, y + 400, x + 1030, y + 380);
-
   pop();
 }
 
@@ -69,7 +86,7 @@ function mapThree(x, y) {
 
   //map
   rect(x + 650, y + 470, 100, 120, 1);
-  rect(x + 820, y + 400, 350, 30, 1);
+  rect(x + 820, y + 400, 190, 30, 1);
   //
   beginShape();
   vertex(x + 310, y + 580);
@@ -125,22 +142,114 @@ function levelThree() {
   push();
   noStroke();
   background(0, 0, 0);
-
-  for (let index in starX) {
-    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
-    ellipse(starX[index], starY[index], 1);
-
-    starAlpha[index] = starAlpha[index] + 0.03;
-  }
+  liquidThree(0, 0);
+  ///////////////
+  //box 1
+  fill(0, 0, 0);
+  rect(b1X, b1Y, bWidth, bHeight);
   pop();
 
-  liquidThree(0, 0);
   mapThree(0, 0);
   borderThree(0, 0);
   obstaclesThree(0, 0);
   doorThree(0, 0);
+  ///////////////
+  //jumping on the box on the left top
+  if (p1X >= 0 && p1X <= 330 && p1Y >= 250 && p1Y <= 300 && jump === false) {
+    p1Y = p1Y; // don't fall through
+    velocity = 0;
+    jumpCounter = 0;
+  }
+  //jumping on the first box
+  if (
+    p1X >= 290 &&
+    p1X <= 400 &&
+    p1Y >= 420 &&
+    p1Y <= minHeight &&
+    jump === false
+  ) {
+    p1Y = p1Y; // don't fall through
+    velocity = 0;
+    jumpCounter = 0;
+  }
+  // jumping on the second box
+  if (
+    p1X >= 380 &&
+    p1X <= 600 &&
+    p1Y >= 340 &&
+    p1Y <= minHeight &&
+    jump === false
+  ) {
+    p1Y = p1Y; // don't fall through
+    velocity = 0;
+    jumpCounter = 0;
+  }
+  // jumping on the third box
+  if (
+    p1X >= 630 &&
+    p1X <= 750 &&
+    p1Y >= 410 &&
+    p1Y <= minHeight &&
+    jump === false
+  ) {
+    p1Y = p1Y; // don't fall through
+    velocity = 0;
+    jumpCounter = 0;
+  }
+  // jumping on the small floating box on the right
+  if (p1X >= 800 && p1X <= 1000 && p1Y >= 340 && p1Y <= 380 && jump === false) {
+    p1Y = p1Y; // don't fall through
+    velocity = 0;
+    jumpCounter = 0;
+  }
+
+  push();
+  // character
+  fill(255, 255, 255);
+  rect(p1X, p1Y, pWidth, pHeight);
+  pop();
+}
+
+function gravity() {
+  if (p1Y >= minHeight && jump === false) {
+    //stop falling on the ground
+    p1Y = p1Y;
+    jumpCounter = 0; //reset jumpCounter
+  } else {
+    p1Y = p1Y + direction * velocity; //makes gravity work
+  }
+
+  if (jump === true) {
+    if (p1Y <= maxHeight || jumpCounter >= jumpPower) {
+      if (p1Y >= minHeight) {
+        p1Y = minHeight; //stay on ground after jumping
+      }
+      velocity = fallingSpeed; // fall at maximums
+    } else {
+      velocity = -jumpPower; //negative is up
+      jumpCounter = jumpCounter + 1; //add to jump counter
+    }
+  } else {
+    velocity = fallingSpeed;
+  }
+}
+
+function keyPressed() {
+  if (keyIsDown(37)) {
+    p1X = p1X - 5; //move left
+  } else if (keyIsDown(39)) {
+    p1X = p1X + 5; // move right
+  }
+
+  if (keyIsDown(38)) {
+    jump = true;
+  } else {
+    jump = false;
+  }
 }
 
 function draw() {
   levelThree();
+  keyPressed();
+  gravity();
 }
